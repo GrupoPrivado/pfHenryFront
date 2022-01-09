edc135bb986b2a4adfb5b25388a045e139ac6f1d
Lulipop4 <lucilamfortunato@gmail.com>
2022-01-09 16:18:19 -0300

    changes in dashboard

--- src/Pages/DashAfiliado/DashAfil.jsx
+++ src/Pages/DashAfiliado/DashAfil.jsx
@@ -1,19 +1,25 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate, Link } from 'react-router-dom';
 import { getAfiliate, getItem, removeItem } from '../../actions/actionAuth';
 import { logout } from '../../utils/authUtils';
+import {  Route, Routes } from "react-router-dom";
 
 import NavBarDashboard from "./../../Components/NavBarDashboard/NavBarDashboard"
 import { getGroup } from '../../actions/actionGroup'
 import FamilyGroupDash from '../../Components/FamilyGroup/FamilyGroupDash'
+import FamilyGroupDetail from '../../Components/FamilyGroup/FamilyGroupDetail';
+import Credencial from '../../Components/Credencial/Credencial';
+import Authorizations from '../../Components/Authorizations/Authorizations';
+import MedicalHistory from '../../Components/MedicalHistory/MedicalHistory';
 
 function DashAfil() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     
     const {user, route} = useSelector(state => state.auth)
- 
+    const [dash, setDash] = useState()
+
     
    // console.log(user, ' <<<<< user >')
     
@@ -33,13 +39,11 @@ function DashAfil() {
     return (
         <div>
             <NavBarDashboard/>
+            <h1>Bienvenidx {user.nombre}</h1>
             <FamilyGroupDash/>
-
-            <p>{user.nombre}</p>
-            <button onClick={() => { logout(); navigate('/') }}>Cerrar Sesión</button>
             
             <Link to='/afiliado/credencial'>
-            <button> Credencial</button>
+                <button> Credencial</button>
             </Link>
 
         </div>
