8e733aba28133d5129f4b3ca19658e9df2b32d97
Felipe Machado <66793977+Ferrama@users.noreply.github.com>
2022-01-09 11:56:17 -0300

    change name var

--- src/Components/FamilyGroup/FamilyGroupDetail.jsx
+++ src/Components/FamilyGroup/FamilyGroupDetail.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import NavbarDasboard from '../NavBarDashboard/NavBarDashboard'
-
+import CardMemberGroup from './CardMemberGroup';
+import { Link } from "react-router-dom";
 export default function FamilyGroupDetail() {
-    const { grupo } = useSelector((state) => state.grupos);
+    const { group } = useSelector((state) => state.grupos);
     return (
         <div>
         <NavbarDasboard/>
-        
-            {grupo && grupo.map(e => (
-                    <div>{e.nombre}</div>
+
+            {group && group.map(e => (
+                    <CardMemberGroup
+                    name={e.nombre}
+                    lastname={e.apellido}
+                    dni={e.DNI}
+                    id={e._id}/>
             ))}
-            
+            <Link to='/afiliado'>
+            <button>Volver</button></Link>
         </div>
     )
 }
