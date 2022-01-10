4e8d43c0598f7cf406cc65db51d5130169b190c2
Alan <56036373+AlanJSx@users.noreply.github.com>
2022-01-10 10:22:33 -0300

    change # to slash href

--- src/Components/NavBarDashboard/NavBarDashboard.jsx
+++ src/Components/NavBarDashboard/NavBarDashboard.jsx
@@ -97,7 +97,7 @@ export default function NavBarDashboard() {
                                                         <Menu.Item>
                                                             {({ active }) => (
                                                                 <a
-                                                                    href="#"
+                                                                    href="/"
                                                                     className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                                                 >
                                                                     Perfil
@@ -107,7 +107,7 @@ export default function NavBarDashboard() {
                                                         <Menu.Item>
                                                             {({ active }) => (
                                                                 <a
-                                                                    href="#"
+                                                                    href="/"
                                                                     className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                                                     onClick={() => { logout(); navigate('/') }}
                                                                 >
