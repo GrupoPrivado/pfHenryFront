2fd8a924a19353feb3d4daae32f8aa1c7ec310e7
Lulipop4 <lucilamfortunato@gmail.com>
2022-01-12 22:19:06 -0300

    profile edit

--- src/Components/Layout/Afiliado.jsx
+++ src/Components/Layout/Afiliado.jsx
@@ -6,6 +6,7 @@ import Authorizations from '../Authorizations/Authorizations'
 import Credencial from '../Credencial/Credencial'
 import FamilyGroupDetail from '../FamilyGroup/FamilyGroupDetail'
 import MedicalHistoryDetails from '../MedicalHistory/MedicalHistoryDetails'
+import Perfil from '../Perfil/Perfil'
 import NavBarDashboard from '../NavBarDashboard/NavBarDashboard'
 import PrivateRouter from '../PrivateRouter/PrivateRouter'
 import CartPrest from '../Providers/CartPrest'
@@ -23,13 +24,13 @@ const Afiliado = () => {
             </PrivateRouter>
           }
         />
-        <Route path="/group" element={<PrivateRouter><FamilyGroupDetail /></PrivateRouter> } />
-        <Route path="/credencial" element={<PrivateRouter><Credencial /></PrivateRouter>} />
-        <Route path="/autorizaciones" element={<PrivateRouter><Authorizations /></PrivateRouter>} />
-        <Route path="/historial" element={<PrivateRouter><MedicalHistoryDetails /></PrivateRouter>} />
-        <Route path="/prestadores" element={<PrivateRouter><CartPrest /></PrivateRouter>} />
+        <Route path="/group" element={<PrivateRouter rol={roles.AFIL}><FamilyGroupDetail /></PrivateRouter>} />
+        <Route path="/credencial" element={<PrivateRouter rol={roles.AFIL}><Credencial /></PrivateRouter>}/>
+        <Route path="/autorizaciones" element={<PrivateRouter rol={roles.AFIL}><Authorizations /></PrivateRouter>} />
+        <Route path="/historial" element={<PrivateRouter rol={roles.AFIL}><MedicalHistoryDetails /></PrivateRouter>} />
+        <Route path="/prestadores" element={<PrivateRouter rol={roles.AFIL}><CartPrest /></PrivateRouter>} />
+        <Route path="/perfil" element={<PrivateRouter rol={roles.AFIL}><Perfil/></PrivateRouter>} />
         <Route path='*' element={<div><h1>Recurso no encontrado</h1></div>} />
-
       </Routes>
     </div>
   );
