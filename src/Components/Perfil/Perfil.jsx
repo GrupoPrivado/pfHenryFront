2fe766908168345efa75773d6339176190fab0ad
Alan <56036373+AlanJSx@users.noreply.github.com>
2022-01-16 11:21:23 -0300

    fix

--- src/Components/Perfil/Perfil.jsx
+++ src/Components/Perfil/Perfil.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { getAfiliate, getItem, removeItem } from '../../actions/actionAuth'
 import {alertActions} from '../../actions/actionAlerts'
 
