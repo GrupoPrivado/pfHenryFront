2846abf945e771d34c17cb935e661db0619ec3df
Alan <56036373+AlanJSx@users.noreply.github.com>
2022-01-20 14:15:32 -0300

    remove deprecated constant "provincias"

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,31 +1,6 @@
 import photo from "../assets/logo_photo.png";
 import date from "./date.js"
 
-export const provincias = [
-    { codeProv: "111", provincia: "Buenos Aires" },
-    { codeProv: "122", provincia: "Catamarca" },
-    { codeProv: "133", provincia: "Chaco" },
-    { codeProv: "144", provincia: "Chubut" },
-    { codeProv: "155", provincia: "Córdoba" },
-    { codeProv: "166", provincia: "Corrientes" },
-    { codeProv: "177", provincia: "Entre Ríos" },
-    { codeProv: "188", provincia: "Jujuy" },
-    { codeProv: "199", provincia: "La Pampa" },
-    { codeProv: "211", provincia: "La Rioja" },
-    { codeProv: "222", provincia: "Mendoza" },
-    { codeProv: "233", provincia: "Misiones" },
-    { codeProv: "244", provincia: "Neuquén" },
-    { codeProv: "255", provincia: "Río Negro" },
-    { codeProv: "266", provincia: "Salta" },
-    { codeProv: "277", provincia: "San Juan" },
-    { codeProv: "288", provincia: "San Luis" },
-    { codeProv: "299", provincia: "Santa Cruz" },
-    { codeProv: "311", provincia: "Santa Fe" },
-    { codeProv: "322", provincia: "Santiago del Estero" },
-    { codeProv: "333", provincia: "Tierra del Fuego" },
-    { codeProv: "344", provincia: "Tucumán" },
-];
-
 export const profilePhoto = photo;
 
 export const validate = (input) => {
