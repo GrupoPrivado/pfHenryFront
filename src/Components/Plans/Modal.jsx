3af74fadcef9f1a2323e4b01ac985613016a4c84
Alan <56036373+AlanJSx@users.noreply.github.com>
2022-01-20 11:31:29 -0300

    fix plans

--- src/Components/Plans/Modal.jsx
+++ src/Components/Plans/Modal.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Modal = ({plan}) => {
+    console.log(plan)
     return (
-        <div className='absolute'>
+        <div className='absolute bg-white'>
             <h2>{plan.name}</h2>
+            {
+                plan.descripcion && plan.descripcion.map((desc, index) => (
+                    <div key={index}> <p> <span>{desc[0]}</span> {desc[1]} </p> </div>
+                ))
+            }
         </div>
     )
 }
