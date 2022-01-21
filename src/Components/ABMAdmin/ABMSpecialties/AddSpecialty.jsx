fef9078b69539d5a09622f6c2360d14f909f9110
DHB1985 <90072728+DHB1985@users.noreply.github.com>
2022-01-21 13:16:38 -0300

    fix addSpeciality

--- src/Components/ABMAdmin/ABMSpecialties/AddSpecialty.jsx
+++ src/Components/ABMAdmin/ABMSpecialties/AddSpecialty.jsx
@@ -1,147 +1,150 @@
-// import React from "react";
+import React from "react";
 
-// import { useState } from "react";
-// import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
-// import {
-//   addSpeciality,
-//   getAllSpecialities,
-// } from "../../../actions/actionAMBAdmin";
+import {
+  addSpeciality,
+  getAllSpecialities,
+} from "../../../actions/actionAMBAdmin";
 
-// import styles from "./addSpecialty.module.css";
-// import { enableBtn, disableBtn } from "../../../utils/ABMStyles";
+import styles from "./addSpecialty.module.css";
+import { enableBtn, disableBtn } from "../../../utils/ABMStyles";
+import {
+  validateAddEspeciality,
+  functionErrorsBtn,
+} from "../../../utils/adminFormsControllers";
 
+const AddSpeciality = ({ setShowModalAdd }) => {
+  const dispatch = useDispatch();
 
-// const AddSpeciality = ({ setShowModalAdd }) => {
-//   const dispatch = useDispatch();
+  const [errors, setErrors] = useState(true);
+  const [errores, setErrores] = useState({});
 
-//   const [errors, setErrors] = useState(true);
-//   const [errores, setErrores] = useState({});
+  const inputSpecialityStruct = {
+    nombre: "",
+    descripcion: "",
+    activa: "",
+  };
 
-//   const inputSpecialityStruct = {
-//     nombre: "",
-//     descripcion: "",
-//     activa: "",
-//   };
+  const [inputSpeciality, setInputSpeciality] = useState(inputSpecialityStruct);
 
-//   const [inputSpeciality, setInputSpeciality] = useState(inputSpecialityStruct);
+  const handleChange = (event) => {
+    let newSpeciality = {
+      ...inputSpeciality,
+      [event.target.name]: event.target.value,
+    };
+    setInputSpeciality(newSpeciality);
 
-//   const handleChange = (event) => {
-//     let newSpeciality = {
-//       ...inputSpeciality,
-//       [event.target.name]: event.target.value,
-//     };
-//     setInputSpeciality(newSpeciality);
+    setErrors(functionErrorsBtn(newSpeciality));
 
-//     setErrors(functionErrorsBtn(newSpeciality));
+    newSpeciality = {};
+  };
 
-//     newSpeciality = {};
-//   };
+  const handleSubmitSpeciality = async (event) => {
+    event.preventDefault();
 
-//   const handleSubmitSpeciality = async (event) => {
-//     event.preventDefault();
+    const validateError = validateAddEspeciality(inputSpeciality);
+    setErrores(validateError);
+    console.log(validateError);
+    if (Object.entries(validateError).length <= 0) {
+      dispatch(addSpeciality(inputSpeciality));
+      //alert(response.success);
+      setInputSpeciality(inputSpecialityStruct);
+      setShowModalAdd(false);
+      //dispatch(getAllSpecialities());
+      setErrors(true);
+    }
+  };
 
-//     const validateError = validateAddEspeciality(inputSpeciality);
-//     setErrores(validateError);
-//     console.log(validateError);
-//     if (Object.entries(validateError).length <= 0) {
-//       dispatch(addSpeciality(inputSpeciality));
-//       //alert(response.success);
-//       setInputSpeciality(inputSpecialityStruct);
-//       setShowModalAdd(false);
-//       //dispatch(getAllSpecialities());
-//       setErrors(true);
-//     }
-//   };
+  const handleClose = () => {
+    setInputSpeciality(inputSpecialityStruct);
+    setShowModalAdd(false);
+    setErrors(true);
+  };
 
-//   const handleClose = () => {
-//     setInputSpeciality(inputSpecialityStruct);
-//     setShowModalAdd(false);
-//     setErrors(true);
-//   };
+  return (
+    <div>
+      <section className={styles.modalmain}>
+        <div className="flex justify-center h-10%">
+          <h5 className="text-2xl font-bold text-gray-500">
+            Agregar Nueva Especialidad
+          </h5>
+        </div>
+        <div className="modal-content py-4 text-left px-6 h-90% ">
+          <form onSubmit={(e) => handleSubmitSpeciality(e)} id="addSpeciality">
+            <div>
+              <label className="text-md text-gray-600">Nombre: </label>
+              <input
+                className="h-3 p-6 w-full border-2 border-gray-300 mb-5 rounded-md"
+                type="text"
+                name="nombre"
+                autoComplete="off"
+                value={inputSpeciality.nombre}
+                onChange={(e) => handleChange(e)}
+                placeholder="Ingrese el nombre...."
+              />
+            </div>
+            {errores.nombre && (
+              <p className="absolute text-red-700">{errores.nombre}</p>
+            )}
 
-//   return (
-//     <div>
-//       <section className={styles.modalmain}>
-//         <div className="flex justify-center h-10%">
-//           <h5 className="text-2xl font-bold text-gray-500">
-//             Agregar Nueva Especialidad
-//           </h5>
-//         </div>
-//         <div className="modal-content py-4 text-left px-6 h-90% ">
-//           <form onSubmit={(e) => handleSubmitSpeciality(e)} id="addSpeciality">
-//             <div>
-//               <label className="text-md text-gray-600">Nombre: </label>
-//               <input
-//                 className="h-3 p-6 w-full border-2 border-gray-300 mb-5 rounded-md"
-//                 type="text"
-//                 name="nombre"
-//                 autoComplete="off"
-//                 value={inputSpeciality.nombre}
-//                 onChange={(e) => handleChange(e)}
-//                 placeholder="Ingrese el nombre...."
-//               />
-//             </div>
-//             {errores.nombre && (
-//               <p className="absolute text-red-700">{errores.nombre}</p>
-//             )}
+            <div>
+              <label className="text-md text-gray-600">Descripción: </label>
+              <input
+                className="h-3 p-6 w-full border-2 border-gray-300 mb-5 rounded-md"
+                type="text"
+                name="descripcion"
+                autoComplete="off"
+                value={inputSpeciality.descripcion}
+                onChange={(e) => handleChange(e)}
+                placeholder="Ingrese la Descripcion...."
+              />
+              {errores.nombre && (
+                <p className="absolute text-red-700">{errores.descripcion}</p>
+              )}
+            </div>
+            <div className="flex justify-between mt-8">
+              <div className="flex w-1/3 items-center">
+                <label className="text-md text-gray-600">Activo: </label>
+                <select
+                  id="activa"
+                  name="activa"
+                  onChange={(e) => handleChange(e)}
+                  defaultValue={0}
+                >
+                  <option value="">Seleccione</option>
+                  <option value="false">No</option>
+                  <option value="true">Si</option>
+                </select>
+                {errores.nombre && (
+                  <p className="absolute text-red-700">{errores.activa}</p>
+                )}
+              </div>
+              <div className="flex w-2/3 justify-around">
+                <button
+                  type="submit"
+                  key="submitFormButton"
+                  form="addSpeciality"
+                  className={errors ? disableBtn : enableBtn}
+                  disabled={errors}
+                >
+                  Guardar
+                </button>
 
-//             <div>
-//               <label className="text-md text-gray-600">Descripción: </label>
-//               <input
-//                 className="h-3 p-6 w-full border-2 border-gray-300 mb-5 rounded-md"
-//                 type="text"
-//                 name="descripcion"
-//                 autoComplete="off"
-//                 value={inputSpeciality.descripcion}
-//                 onChange={(e) => handleChange(e)}
-//                 placeholder="Ingrese la Descripcion...."
-//               />
-//               {errores.nombre && (
-//                 <p className="absolute text-red-700">{errores.descripcion}</p>
-//               )}
-//             </div>
-//             <div className="flex justify-between mt-8">
-//               <div className="flex w-1/3 items-center">
-//                 <label className="text-md text-gray-600">Activo: </label>
-//                 <select
-//                   id="activa"
-//                   name="activa"
-//                   onChange={(e) => handleChange(e)}
-//                   defaultValue={0}
-//                 >
-//                   <option value="">Seleccione</option>
-//                   <option value="false">No</option>
-//                   <option value="true">Si</option>
-//                 </select>
-//                 {errores.nombre && (
-//                   <p className="absolute text-red-700">{errores.activa}</p>
-//                 )}
-//               </div>
-//               <div className="flex w-2/3 justify-around">
-//                 <button
-//                   type="submit"
-//                   key="submitFormButton"
-//                   form="addSpeciality"
-//                   className={errors ? disableBtn : enableBtn}
-//                   disabled={errors}
-//                 >
-//                   Guardar
-//                 </button>
+                <button
+                  onClick={() => handleClose()}
+                  className="group relative w-15 h-10 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Cerrar
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-//                 <button
-//                   onClick={() => handleClose()}
-//                   className="group relative w-15 h-10 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-//                 >
-//                   Cerrar
-//                 </button>
-//               </div>
-//             </div>
-//           </form>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default AddSpeciality;
+export default AddSpeciality;
