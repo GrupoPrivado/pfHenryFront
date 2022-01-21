b1bb3694f0c3d39373005c91c0028e36cf675055
DHB1985 <90072728+DHB1985@users.noreply.github.com>
2022-01-21 11:57:34 -0300

    Fix app

--- src/App.js
+++ src/App.js
@@ -10,26 +10,11 @@ function App() {
   return (
     <Fragment>
       <Routes>
-<<<<<<< HEAD
-        <Route path="/*" element={<Guest />} />
-        <Route path="afiliado/*" element={<Afiliado />} />
-        <Route path="administrador/*" element={<Admin />} />
-        <Route path="profesional/*" element={<Profesionales />} />
-        <Route
-          path="*"
-          element={
-            <div>
-              <h1>Recurso no encontrado</h1>
-            </div>
-          }
-        />
-=======
           <Route path='/*' element={<Guest />} />
           <Route path='afiliado/*' element={<Afiliado/>} />
           <Route path='administrador/*' element={<Admin/>} />
           <Route path='profesional/*' element={<Profesionales/>} />
         <Route path='*' element={<div><h1>Recurso no encontrado</h1></div>} />
->>>>>>> 5dc21c3416b76effffb8674f37bebe3437a755dc
       </Routes>
       {/* <Footer /> */}
     </Fragment>
